c01fdedccfe0dd131f5d2fca48790a0994c43b95
fernbern <70728008+fernbern@users.noreply.github.com>
2020-09-15 15:42:59 -0700

    First Block Try

---
+++ ga-gutenberg-blocks.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+Plugin Name: Gourmet Artist Gutenburg Blocks
+Plugin URI: 
+Description: Add Gutenburg Blocks
+Version: 1.0
+Author: Fernando Castanon
+Author URI:
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+// Prevent exucution
+if(!defined('ASBPATH')) exit;
+
+
+?>
